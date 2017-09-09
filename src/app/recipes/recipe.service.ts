883b88fd7417251830c969641027076f2e36bab0
bumpMind <damian.s.ja@gmail.com>
2017-09-09 11:03:45 +0200

    Added ingredients to recipe and show them in recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,37 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-		new Recipe('Simple Recipe', 'This is a simple recipe', 'https://www.bbcgoodfood.com/sites/default/files/styles/carousel_medium/public/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg?itok=naGPMoRQ'),
-		new Recipe('Pierogi', 'This is a simple recipe for pierogi', 'http://www.zajadam.pl/wp-content/uploads/2015/01/pierogi-z-kasza-2-891x500.jpg'),
-		new Recipe('Pizza', 'Simple as...', 'http://www.zajadam.pl/wp-content/uploads/2015/02/pizza-pepperoni-9-469x313.jpg')
+		new Recipe(
+			'Simple Recipe',
+			'This is a simple recipe',
+			'https://www.bbcgoodfood.com/sites/default/files/styles/carousel_medium/public/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg?itok=naGPMoRQ',
+			[
+				new Ingredient('Potato', 2),
+				new Ingredient('Chicken', 1)
+			]),
+		new Recipe(
+			'Pierogi',
+			'This is a simple recipe for pierogi',
+			'http://www.zajadam.pl/wp-content/uploads/2015/01/pierogi-z-kasza-2-891x500.jpg',
+			[
+				new Ingredient('Cus', 3),
+				new Ingredient('Cus innego', 10)
+			]),
+		new Recipe(
+			'Pizza',
+			'Simple as...',
+			'http://www.zajadam.pl/wp-content/uploads/2015/02/pizza-pepperoni-9-469x313.jpg',
+			[
+				new Ingredient('Tomato', 5),
+				new Ingredient('Fries', 12)
+			]
+		)
 	];
 
 
