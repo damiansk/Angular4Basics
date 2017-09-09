0789c8280dc8ec735350d5da7f1f6d61ddd35b83
bumpMind <damian.s.ja@gmail.com>
2017-09-09 11:22:56 +0200

    Added functionality of add recipe ingredients to shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -21,4 +21,9 @@ export class ShoppingListService {
 		this.ingredientsChanged.emit(this.getIngredients());
 	}
 
+	addIngredients(ingredients: Ingredient[]) {
+		this.ingredients.push(...ingredients);
+		this.ingredientsChanged.emit(this.getIngredients());
+	}
+
 }
